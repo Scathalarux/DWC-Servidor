84f190468f3b641f9bfb9cb32a574292332af045
LaraCV <laracome1998@gmail.com>
2024-10-08 20:44:43 +0200

    Ejercicio 8 terminado

--- sites/UD3/www/public/ud3/controllers/iterativas08.controller.php
+++ sites/UD3/www/public/ud3/controllers/iterativas08.controller.php
@@ -1,72 +1,108 @@
 <?php
 declare(strict_types=1);
 //var_dump(mb_internal_encoding());
+//$data=array();
+//var_dump($_POST);
 
 //si no han enviado algo
-if(!empty($_POST)){
-    //saneamos
-    $data['input_texto'] = filter_var($_POST["texto"], FILTER_SANITIZE_SPECIAL_CHARS);
-    //validamos el formulario
-    $errors = checkJson($_POST['texto']);
+if (!empty($_POST)) {
+  //saneamos
+  $data['input_texto'] = filter_var($_POST["texto"], FILTER_SANITIZE_SPECIAL_CHARS);
+  //validamos el formulario
+  $errors = checkJson($_POST['texto']);
 
-    //hay o no hay errores
-    if(count($errors) > 0){
-        $data['errors'] = $errors;
-    }else{
+  //hay o no hay errores
+  if (count($errors) > 0) {
+    $data['errors'] = $errors;
+  } else {
     //procesamiento
-        $data['json'] =json_decode($_POST['texto'], true);;
-        foreach ($array as $key => $value) {
-            $data['asignatura'] = $key;
-            foreach($value as $alum => $nota){
-                $data['alumno']= $alum;
-                $data['nota'] = $nota;
-            }
-        }
+    $data['json'] = json_decode($_POST['texto'], true);
+    $data['json'] = calcSubject($data['json']);
+  }
+}
+
+//funcion que calcula la nota media de cada asignatura
+//función que calcula los suspensos
+//función que calcula los aprobados
+//función que calcula el alumno con mejor nota
+//función que calcula el alumno con peor nota
+function calcSubject(array $json): array
+{
+  $resultados = [];
+  foreach ($json as $asignaturas => $alumnos) {
+    $asignatura = [];
+    $conjuntoNotas = 0;
+    $asignatura['suspensos'] = 0;
+    $asignatura['aprobados'] = 0;
+
+    foreach ($alumnos as $alum => $nota) {
+      $conjuntoNotas += $nota;
+      if ($nota >= 5) {
+        $asignatura['aprobados']++;
+      } else {
+        $asignatura['suspensos']++;
+      }
+      if ($nota === max($alumnos)) {
+        $alumnoMax['alumno'] = $alum;
+        $notaMax['nota'] = $nota;
+        $asignatura['max'] = array_merge($alumnoMax, $notaMax);
+      }
+      if ($nota === min($alumnos)) {
+        $alumnoMin['alumno'] = $alum;
+        $notaMin['nota'] = $nota;
+        $asignatura['min'] = array_merge($alumnoMin, $notaMin);
+      }
     }
 
+    $asignatura['media'] = $conjuntoNotas / count($alumnos);
+    $resultados[$asignaturas] = $asignatura;
+  }
+  var_dump($resultados);
+  return $resultados;
 }
 
 //funcion que valida si el json es correcto o presenta algún error
-function checkJson (string $text):array{
-    $errors= [];
+function checkJson(string $text): array
+{
+  $errors = [];
 
-    if(empty($text)){
-        $errors['texto'][] = 'Inserte un json a analizar';
-    }else{
-        $array = json_decode($text, true);
+  if (empty($text)) {
+    $errors['texto'][] = 'Inserte un json a analizar';
+  } else {
+    $array = json_decode($text, true);
 
-        //comprobamos si hay errores de procesamiento del json
-        if(is_null($array)){
-            $errors['texto'][] = 'json incorrecto, introduce un json válido';
-        }else{
-            if(!is_array($array)){
-                $errors['texto'][] = 'json incorrecto, introduce un array de asignaturas';
-            }else{
+    //comprobamos si hay errores de procesamiento del json
+    if (is_null($array)) {
+      $errors['texto'][] = 'json incorrecto, introduce un json válido';
+    } else {
+      if (!is_array($array)) {
+        $errors['texto'][] = 'json incorrecto, introduce un array de asignaturas';
+      } else {
 
-                //comprobamos si la estructura sigue el patrón texto[texto:num(0-10)]
-                foreach ($array as $asignaturas => $alumnos) {
-                    //comprobamos que la asingatura sea texto
-                    if(!is_string($asignaturas) || mb_strlen(trim($asignaturas)) < 1){
-                        $errors['texto'][]= "El nombre de la asignatura '$asignaturas' no es válido, debe ser un texto";
-                    }elseif(!is_array($alumnos)){
-                        $errors['texto'][]= "Cada asignatura debe contener un conjunto de alumnos con sus respectivas asignaturas. Tienes un error en el array $alumnos";
-                    }else{
-                        //si la asignatura es tipo texto
-                        foreach ($alumnos as $alum => $nota) {
-                            if (!is_string($alum) || mb_strlen(trim($alum)) < 1) {
-                                $errors['texto'][] = "El nombre del alumno '$alum' no es válido. Tienes un error en '$asignaturas'";
-                            } elseif (!is_numeric($nota)) {
-                                $errors['texto'][] = "La nota debe ser un número. Tienes un error en la nota '$nota' del alumno '$alum'";
-                            } elseif (($nota > 10) || ($nota < 0)) {
-                                $errors['texto'][] = 'La nota debe estar entre 0 y 10. Tienes un error en la nota del alumno: ' . $alum;
-                            }
-                        }
-                    }
-                }
+        //comprobamos si la estructura sigue el patrón texto[texto:num(0-10)]
+        foreach ($array as $asignaturas => $alumnos) {
+          if (!is_string($asignaturas) || mb_strlen(trim($asignaturas)) < 1) {
+            $errors['texto'][] = "El nombre de la asignatura '$asignaturas' no es válido, debe ser un texto";
+          }
+          if (!is_array($alumnos)) {
+            $errors['texto'][] = "Cada asignatura debe contener un conjunto de alumnos con sus respectivas notas. Tienes un error en el array $asignaturas";
+          } else {
+            foreach ($alumnos as $alum => $nota) {
+              if (!is_string($alum) || mb_strlen(trim($alum)) < 1) {
+                $errors['texto'][] = "El nombre del alumno '$alum' no es válido. Tienes un error en '$asignaturas'";
+              }
+              if (!is_numeric($nota)) {
+                $errors['texto'][] = "La nota debe ser un número. Tienes un error en la nota '$nota' del alumno '$alum'";
+              } else if (($nota > 10) || ($nota < 0)) {
+                $errors['texto'][] = 'La nota debe estar entre 0 y 10. Tienes un error en la nota del alumno: ' . $alum;
+              }
             }
+          }
         }
+      }
     }
-    return $errors;
+  }
+  return $errors;
 }
 
 /*
--- sites/UD3/www/public/ud3/views/iterativas08.view.php
+++ sites/UD3/www/public/ud3/views/iterativas08.view.php
@@ -1,4 +1,3 @@
-
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Iterativas 08</h1>
@@ -6,21 +5,38 @@
 </div>
 <!-- Content Row -->
 
-<?php if (isset($data['alumno'])) { ?>
+<?php if (isset($data['json'])) { ?>
     <div class="row">
         <div class="col-12">
             <div class="alert alert-success">
-                <table>
-                    <thead> Contenido JSON</thead>
-                    <tbody>
+                <table style="border: 1px solid black; text-align: left">
+                    <tbody style="border: 1px solid black">
+                    <?php foreach ($data['json'] as $asignatura => $datos) { ?>
                         <tr>
-                            <th>Asignatura</th>
-                            <th>Alumno</th>
-                            <th>Nota</th>
+                            <td style="padding: 10px">
+                              <?php echo $asignatura ?>
+                            </td>
+                            <td style="padding: 10px"> Media:
+                              <?php echo $datos['media'] ?>
+                            </td>
+                            <td style="padding: 10px"> Suspensos:
+                              <?php echo $datos['suspensos'] ?>
+                            </td>
+                            <td style="padding: 10px"> Aprobados:
+                              <?php echo $datos['aprobados'] ?>
+                            </td>
+                            <td style="padding: 10px"> Max:
+                              <?php foreach ($datos['max'] as $key => $value) {
+                                echo $key . ": " . $value . "<br/>";
+                              } ?>
+                            </td>
+                            <td style="padding: 10px"> Min:
+                              <?php foreach ($datos['min'] as $key => $value) {
+                                echo $key . ": " . $value . "<br/>";
+                              } ?>
+                            </td>
                         </tr>
-                    <tr>
-                        <td></td>
-                    </tr>
+                    <?php }; ?>
                     </tbody>
                 </table>
                 <p></p>
@@ -28,7 +44,7 @@
             </div>
         </div>
     </div>
-    <?php
+  <?php
 }
 ?>
 <div class="row">
@@ -43,9 +59,13 @@
                         <label for="textarea">Introduce el JSON: </label>
                         <textarea class="form-control" id="texto" name="texto"
                                   rows="3"><?php echo $data['input_texto'] ?? ''; ?></textarea>
-                        <p class="text-danger small"><?php echo $data['errors']['texto'] ?? ''; ?></p>
-                        <br />
-                                            </div>
+                        <p class="text-danger small">
+                          <?php foreach ($data['errors']['texto'] as $key => $value) {
+                            echo $value . "<br/>";
+                          }
+                          ?></p>
+                        <br/>
+                    </div>
                     <div class="mb-3">
                         <input type="submit" value="Enviar" name="enviar" class="btn btn-primary">
                     </div>
