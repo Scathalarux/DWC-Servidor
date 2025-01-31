54c8a0b59e941d2203d02a84e31a2ad0f4f26e39
LaraCV <laracome1998@gmail.com>
2025-01-31 09:53:54 +0100

    continuacion proveedores

--- sites/ud7-base/www/app/Controllers/ProveedorController.php
+++ sites/ud7-base/www/app/Controllers/ProveedorController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Com\Daw2\Controllers;
 
 use Com\Daw2\Core\BaseController;
+use Com\Daw2\Libraries\Respuesta;
 use Com\Daw2\Models\ProveedorModel;
 
 class ProveedorController extends BaseController
@@ -15,35 +16,67 @@ class ProveedorController extends BaseController
     private const DEFAULT_ORDER = 1;
     public function login(): void
     {
-
     }
 
     public function listarProveedor(): void
     {
         $modelProveedor = new ProveedorModel();
+        //obtención del campo para la ordenación
+        $order = $this->getOrder();
+
+        //obtención de a página máxima que se puede alcanzar
+        $maxPage = $modelProveedor->getMaxPage($_GET, self::SIZE_PAGE);
+        //obtención de la página de la que se quieren mostrar los resultados
+        $page = $this->getPage($maxPage);
+
+        $proveedores = $modelProveedor->listProveedoresFiltered($_GET, $order, $page, $maxPage);
+
+        if($proveedores === false){
+            $respuesta = new Respuesta(400, ['mensaje'=>'ne se han podido encontrar proveedores con esas características']);
+        }else{
+            $respuesta = new Respuesta(200, $proveedores);
+        }
 
 
-        $this->view->show();
+
+        $this->view->show('jsonProveedor.view.php', ['respuesta' => $respuesta]);
+    }
+
+    public function getOrder(): int
+    {
+        if (isset($_GET['order']) && filter_var($_GET['order'], FILTER_VALIDATE_INT)) {
+            if (abs((int)$_GET['order']) > 0 && (abs((int)$_GET['order'])) <= ProveedorModel::COLUMNS_ORDER) {
+                return (int)$_GET['order'];
+            }
+        }
+
+        return self::DEFAULT_ORDER;
+    }
+
+    public function getPage($maxPage): int
+    {
+        if (isset($_GET['page']) && filter_var($_GET['page'], FILTER_VALIDATE_INT)) {
+            if ((int)$_GET['pge'] > 0 && (int)$_GET['order'] <= $maxPage) {
+                return (int)$_GET['page'];
+            }
+        }
+
+        return self::DEFAULT_PAGE;
     }
 
     public function getProveedor(string $cif): void
     {
-
     }
 
     public function addProveedor(): void
     {
-
     }
 
     public function deleteProveedor(string $cif): void
     {
-
     }
 
     public function editProveedor(string $cif): void
     {
-
     }
-
 }
--- sites/ud7-base/www/app/Models/ProveedorModel.php
+++ sites/ud7-base/www/app/Models/ProveedorModel.php
@@ -8,11 +8,121 @@ use Com\Daw2\Core\BaseDbModel;
 
 class ProveedorModel extends BaseDbModel
 {
+    private const SELECT_BASE = 'SELECT pv.*, (SELECT pd.* FROM producto pd WHERE pd.proveedor =  pv.cif) as total_productos_proveedor' . self::FROM;
+    private const FROM = ' FROM proveedor pv ';
+
+    private const COUNT_BASE = 'SELECT COUNT(*) ' . self::FROM;
+    public const COLUMNS_ORDER = ['cif','codigo','nombre', 'pais', 'total_productos_proveedor'];
     public function find(string $cif): false|array
     {
         $sql = "SELECT * FROM proveedor WHERE cif = :cif";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute(['cif' => $cif]);
         return $stmt->fetch();
+    }
+
+
+    public function listProveedoresFiltered(array $data, int $order, int $page, int $maxPage): array|false
+    {
+        $filtros = $this->filtrosQuery($data);
+
+        $sentido = $order > 0 ? ' ASC ' : ' DESC ';
+
+        $order = abs($order);
+
+        $offset = ($page - 1) * $maxPage;
+
+        if ($filtros['conditions'] !== []) {
+            $sql = self::SELECT_BASE
+                . " WHERE " . implode(' AND ', $filtros['conditions'])
+                . ' ORDER BY ' . self::COLUMNS_ORDER[$order - 1] . $sentido
+                . " LIMIT $offset, $maxPage";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($filtros['vars']);
+        } else {
+            $stmt = $this->pdo->query(self::SELECT_BASE
+                                        . ' ORDER BY ' . self::COLUMNS_ORDER[$order - 1] . $sentido
+                                        . " LIMIT $offset, $maxPage");
+        }
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function filtrosQuery(array $data): array
+    {
+        $conditions = [];
+        $vars = [];
+
+        //cif
+        if (!empty($data['cif'])) {
+            $conditions[] = ' cif LIKE :cif';
+            $cif = $data['cif'];
+            $vars['cif'] = "%$cif%";
+        }
+
+        //nombre
+        if (!empty($data['nombre'])) {
+            $conditions[] = ' nombre LIKE :nombre';
+            $nombre = $data['nombre'];
+            $vars['nombre'] = "%$nombre%";
+        }
+
+        //codigo
+        if (!empty($data['codigo'])) {
+            $conditions[] = ' codigo LIKE :codigo';
+            $codigo = $data['codigo'];
+            $vars['codigo'] = "%$codigo%";
+        }
+
+        //email
+        if (!empty($data['email'])) {
+            $conditions[] = ' email LIKE :email';
+            $email = $data['email'];
+            $vars['email'] = "%$email%";
+        }
+
+        //pais
+        if (!empty($data['pais'])) {
+            $conditions[] = ' pais = :pais';
+            $vars['pais'] = $data['pais'];
+        }
+
+        //productos minimos
+        if (!empty($data['min_productos'])) {
+            $conditions[] = ' total_productos_proveedor >=:min_productos';
+            $vars['min_productos'] = $data['min_productos'];
+        }
+
+        //productos máximos
+        if (!empty($data['max_productos'])) {
+            $conditions[] = ' total_productos_proveedor >=:max_productos';
+            $vars['max_productos'] = $data['max_productos'];
+        }
+
+        $respuesta['conditions'] = $conditions;
+        $respuesta['vars'] = $vars;
+
+        return $respuesta;
+    }
+
+    public function getMaxPage(array $data, int $sizePage): int
+    {
+        //realizamos una consulta siguiendo los filtros introducidos
+        //calculamos el número de filas obtenidas
+        //hacemos la repartición de filas entre las páginas según el tamaño indicado
+
+        $filtros = $this->filtrosQuery($data);
+
+        if ($filtros['conditions'] !== []) {
+            $sql = self::COUNT_BASE
+                . " WHERE " . implode(' AND ', $filtros['conditions'])
+               ;
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($filtros['vars']);
+        } else {
+            $stmt = $this->pdo->query(self::COUNT_BASE);
+        }
+        $rows =  $stmt->fetchColumn(0);
+
+        return (int)ceil($rows / $sizePage);
     }
 }
